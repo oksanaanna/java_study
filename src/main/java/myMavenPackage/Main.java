489f761b7195d394329d6297c75bd9609a5383f3
Oksana <oksanaanna@yahoo.com>
2018-08-15 20:16:45 +0300

    my seventh commit

--- src/main/java/myMavenPackage/Main.java
+++ src/main/java/myMavenPackage/Main.java
@@ -2,20 +2,7 @@ package myMavenPackage;
 
 public class Main {
     public static void main(String[] args) {
-        TestClass testClass = new TestClass();
-        MyAnnotation annotation = testClass.getClass().getAnnotation(MyAnnotation.class);
-        System.out.println(annotation.color());
-        LuxuryLevel annotation1 = testClass.getClass().getAnnotation(LuxuryLevel.class);
-        //System.out.println(annotation1.boat());
     }
 }
-class Master {
-    protected static void High (boolean t){
-        System.out.println(t);
-    }
-} interface G {
-
-    }
-
 
 
