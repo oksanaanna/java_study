608bcf7c82aa23e4dab33c4c446fd92015424d4e
Oksana <oksanaanna@yahoo.com>
2018-08-15 20:42:39 +0300

    my fourteen commit

--- src/main/java/myMavenPackage/Main.java
+++ src/main/java/myMavenPackage/Main.java
@@ -2,11 +2,16 @@ package myMavenPackage;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
         int q = 1;
         System.out.println(q);
     }
 } class Friend{
-    void True (boolean a){}
+    void True (boolean a){
+
+    }
 
 }
 
